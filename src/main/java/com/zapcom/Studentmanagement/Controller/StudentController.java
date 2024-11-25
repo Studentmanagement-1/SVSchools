ae137089619a31363ea57c762cd707782c436cd1
Thirumalesh Gajula <thirumalesh.gajula@zapcg.com>
2024-11-25 11:28:18 +0530

    secondmessage

--- src/main/java/com/zapcom/Studentmanagement/Controller/StudentController.java
+++ src/main/java/com/zapcom/Studentmanagement/Controller/StudentController.java
@@ -1,14 +1,20 @@
 package com.zapcom.Studentmanagement.Controller;
 
+import com.zapcom.Studentmanagement.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 
+
+    @Autowired
+    private StudentService service;
+
     @GetMapping("/greet")
     public String greetings(){
-        return "hello";
+        return service.greetings();
     }
 
 }
---
+++ src/main/java/com/zapcom/Studentmanagement/Service/StudentService.java
@@ -0,0 +1,12 @@
+package com.zapcom.Studentmanagement.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    public String greetings(){
+        return "hello";
+    }
+
+}
