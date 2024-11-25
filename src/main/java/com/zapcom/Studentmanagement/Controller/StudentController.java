68cb95f5909b8107d0a9b05f86f84e739a3d70b3
Thirumalesh-63 <gtm5562@gmail.com>
2024-11-25 15:49:08 +0530

    13 message

--- src/main/java/com/zapcom/Studentmanagement/Controller/StudentController.java
+++ src/main/java/com/zapcom/Studentmanagement/Controller/StudentController.java
@@ -14,15 +14,13 @@ public class StudentController {
 
     @GetMapping("/greet")
     public String greetings(){
-        System.out.println("morning");
-        System.out.println("morning");
+        System.out.println("hello");
         return service.greetings();
     }
 
     @GetMapping("/greet2")
     public String greetings2(){
-         System.out.println("morning");
-        System.out.println("morning");
+        System.out.println("hello");
         return "Good Morning";
     }
 
