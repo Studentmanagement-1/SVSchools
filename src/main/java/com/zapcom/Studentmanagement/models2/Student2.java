3857e3d35f1136ba4e8aa7b143a4873bf1551955
Thirumalesh-63 <gtm5562@gmail.com>
2024-11-25 14:58:04 +0530

    seventhmessge

---
+++ src/main/java/com/zapcom/Studentmanagement/models2/Student2.java
@@ -0,0 +1,17 @@
+package com.zapcom.Studentmanagement.models2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student2 {
+
+    private int id;
+
+    private String name;
+
+    private int age;
+}
