4e5bddb3582c1100be183bc0d387c1012416f34f
Thirumalesh-63 <gtm5562@gmail.com>
2024-11-26 10:00:43 +0530

    Revert "22message"
    This reverts commit 2af40ebc8711fe598416d5b4041a07b048e77861.

--- src/main/java/com/zapcom/Studentmanagement/Service/StudentService.java
+++ src/main/java/com/zapcom/Studentmanagement/Service/StudentService.java
@@ -18,8 +18,4 @@ public class StudentService {
         System.out.println("Thanks namasthe");
         return "how are you";
     }
-    public String greetings6(){
-        System.out.println("Thanks namasthe6");
-        return "how are you";
-    }
 }
