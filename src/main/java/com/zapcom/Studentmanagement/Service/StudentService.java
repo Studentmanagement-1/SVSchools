d1f2b3d178689f96ab0babc087d4ba049757e2d5
Thirumalesh-63 <gtm5562@gmail.com>
2024-11-26 09:45:55 +0530

    Revert "21 message"
    This reverts commit 153cc96668a7759ed5574afafe909ede28e9f9a9.

--- src/main/java/com/zapcom/Studentmanagement/Service/StudentService.java
+++ src/main/java/com/zapcom/Studentmanagement/Service/StudentService.java
@@ -18,8 +18,4 @@ public class StudentService {
         System.out.println("Thanks namasthe");
         return "how are you";
     }
-    public String greetings5(){
-        System.out.println("Thanks namasthe5");
-        return "how are you";
-    }
 }
