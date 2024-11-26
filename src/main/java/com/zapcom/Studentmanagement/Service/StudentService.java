036793c7657f1f34580c73f8044529262aedff9e
Thirumalesh-63 <129723705+Thirumalesh-63@users.noreply.github.com>
2024-11-26 11:08:42 +0530

    Update StudentService.java

--- src/main/java/com/zapcom/Studentmanagement/Service/StudentService.java
+++ src/main/java/com/zapcom/Studentmanagement/Service/StudentService.java
@@ -12,10 +12,14 @@ public class StudentService {
 
     public String greetings2(){
         System.out.println("Thanks namasthe");
+         System.out.println("namasthe26");
+        System.out.println("namasthe16");
         return "how are you";
     }
     public String greetings4(){
         System.out.println("Thanks namasthe");
+         System.out.println("namasthe26");
+        System.out.println("namasthe16");
         return "how are you";
     }
 }
